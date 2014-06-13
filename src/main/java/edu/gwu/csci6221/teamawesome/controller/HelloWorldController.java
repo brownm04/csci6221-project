7d4e49fb88b8d6afe45519d48b2d61e0da437946
Matt Brown <brown_matthew@bah.com>
2014-06-13 11:17:19 -0400

    Removed old helloWorld model object from controller

--- src/main/java/edu/gwu/csci6221/teamawesome/controller/HelloWorldController.java
+++ src/main/java/edu/gwu/csci6221/teamawesome/controller/HelloWorldController.java
@@ -1,13 +1,10 @@
 package edu.gwu.csci6221.teamawesome.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import edu.gwu.csci6221.teamawesome.model.HelloModel;
 
 @Controller
 public class HelloWorldController {
@@ -15,7 +12,7 @@ public class HelloWorldController {
 	public String hello(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
-		model.addAttribute("name", helloModel.word);
+		model.addAttribute("name", name);
 		return "helloworld";
 	}
 }
